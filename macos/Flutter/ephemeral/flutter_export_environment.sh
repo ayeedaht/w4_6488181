874824b412e4a5b503e14e617f2ccf65800f7803
ayeedaht <thadeeya.dua@gmail.com>
2024-02-08 10:45:53 +0700

    submission

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\ICT Mahidol\Year 3\Term 2\ITCS424 - Wireless\project"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Student\Desktop\project"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
